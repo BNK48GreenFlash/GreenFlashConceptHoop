777f70484322beca924181f06065a538770fa6b7
BNK48GreenFlash <kalosrsxy@gmail.com>
2025-02-04 17:24:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function tweetMessage() {
-    const message = "Hoop GreenFlash\n#HoopBNK48\n#GreenFlashConceptDay1";
+    const message = "Hoop GreenFlash\n#HoopBNK48\n#GreenFlashConceptDay2";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }
